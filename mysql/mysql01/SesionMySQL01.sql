0e3431a3c3142ff258a07870126fb3c0889a07ab
Sergio Torres <sergio.torres@idr.org.mx>
2023-02-15 16:21:10 -0600

    feat: sesion mysql 01

---
+++ mysql/mysql01/SesionMySQL01.sql
@@ -0,0 +1,11 @@
+-- Esto es un comentario de línea
+/* Esto es un comentario de bloque */
+
+-- Crear la base de datos banco
+-- CREATE DATABASE banco;
+-- Usar la base de datos banco
+USE banco;
+-- Mostrar la estructura de la tabla
+DESCRIBE clientes;
+-- Mostrar todos los datos de la tabla clientes
+SELECT * FROM clientes;
